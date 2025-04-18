fcdfcac58c9d50213796c6a8275b2a8b34d0913e
abhishek-1809 <abhisahu1920@gmail.com>
2025-04-18 21:48:16 +0530

    add user_service

---
+++ backend/user-service/src/main/java/com/real_time_auction_system/user_service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.real_time_auction_system.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ backend/user-service/src/test/java/com/real_time_auction_system/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.real_time_auction_system.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
