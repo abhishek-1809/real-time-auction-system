70e858b67228a18572df3dde024bec82a7aa4353
abhishek-1809 <abhisahu1920@gmail.com>
2025-04-21 14:19:53 +0530

    add eureka-service-registry

---
+++ backend/service_registry/src/test/java/com/real_time_auction_system/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.real_time_auction_system.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
